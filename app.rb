9d6ca9b61a30e4baa082a84b33f5ba8607640923
Lin He <he9lin@gmail.com>
2015-12-13 16:25:23 -0800

    Use redis url

--- app.rb
+++ app.rb
@@ -6,18 +6,15 @@ require 'dotenv'
 Dotenv.load
 
 class DeployApp < Sinatra::Base
-
   get '/*' do
     content_type 'text/html'
 
     prefix    = "#{ENV['APP_NAME']}:index"
-    redis     = Redis.new host: ENV['REDIS_HOST'], \
-      port: ENV['REDIS_PORT'], password: ENV['REDIS_SECRET']
+    redis     = Redis.new url: ENV['REDIS_URL']
     index_key = redis.get("#{prefix}:current")
     if params[:index_key]
       index_key = "#{params[:index_key]}"
     end
     redis.get("#{prefix}:#{index_key}")
   end
-
 end
